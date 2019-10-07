77892664076190354f485298a639311b676e8826
kevin <kevin.hernandez1802@gmail.com>
2019-10-07 11:11:36 -0500

    done

--- server/index.js
+++ server/index.js
@@ -9,8 +9,8 @@ const {addForum,getforumPost,getPastThreads,postOnForum,getAllThreads} = require
 
 const app = express()
 
-app.use(express.json())
 app.use( express.static( `${__dirname}/../build` ) );
+app.use(express.json())
 
 const {SERVER_PORT,CONNECTION_STRING} = process.env
 
@@ -29,9 +29,7 @@ app.use(session({
         maxAge: 1000 * 60 * 60 * 24 * 3
     }
 }))
-app.get('*', (req, res)=>{
-    res.sendFile(path.join(__dirname, '../build/index.html'));
-});
+
 
 
 
@@ -62,5 +60,7 @@ app.get("/api/forumPost/:forumid",getforumPost)
 app.post("/api/forumPost/:forumid",postOnForum)
 
 
-
+app.get('*', (req, res)=>{
+    res.sendFile(path.join(__dirname, '../build/index.html'));
+});
 app.listen(SERVER_PORT,()=> console.log(`Listening on port ${SERVER_PORT}`))
