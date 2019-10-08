80d1f66fc5a4b7cc8fe7a819856690eafb7c0ecc
kevin <kevin.hernandez1802@gmail.com>
2019-10-08 15:11:11 -0500

    done

--- db/getAllPost.sql
+++ db/getAllPost.sql
@@ -2,4 +2,4 @@ SELECT users.username,posts.content_of_post,posts.img_url,posts.likes,posts.post
 from users 
 INNER JOIN posts
 ON users.id = posts.post_user_id
-ORDER BY posts.likes DESC
+ORDER BY posts.post_id DESC
